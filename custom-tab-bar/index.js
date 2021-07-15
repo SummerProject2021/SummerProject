499d344783dba23924c796cfb96612898b27ebfe
exit <jimmysoccer0927@gmail.com>
2021-07-15 20:01:34 +0800

    basic layout without logic

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,56 @@
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    _tabbat: 0,
+    iPhoneX: false,
+    urls: [
+      '/pages/index/index',
+      '/pages/logs/logs'
+    ]
+  },
+  attached() {
+    var self = this
+    wx.getSystemInfo({
+      success(res) {
+        console.log(res.model)
+        if (res.model.indexOf('iPhone X') >= 0) {
+          self.setData({
+            iPhoneX: true
+          })
+        }
+      }
+    })
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    switchTap: function (e) {
+      var self = this
+      var index = e.currentTarget.dataset.index;
+      var urls = self.data.urls
+      var x1,y1;
+      x1=e.touches[0].pageX;
+      y1=e.touches[0].pageY;
+      //x1>140&x1<180&(y1>10&y1<20)
+      if (x1<180) {
+      wx.switchTab({
+        url: urls[0]
+      })
+      }else if (x1>=180) {
+        wx.switchTab({
+          url: urls[1]
+        })
+      }
+    }
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,68 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+const query = wx.createSelectorQuery()
+query.select('#the-id').boundingClientRect()
+query.selectViewport().scrollOffset()
+query.exec(function (res) {
+  res[0].top // #the-id节点的上边界坐标
+  res[1].scrollTop // 显示区域的竖直滚动位置
+})
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUseGetUserProfile: false,
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') ,// 如需尝试获取用户信息可改为false,
+    x1:0,
+    y1:0
+  },
+  GetPoint:function(e){
+    this.setData({
+        x1:e.touches[0].pageX,
+        y1:e.touches[0].pageY
+    })
+},
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+  onShow: function () {
+    this.getTabBar().setData({
+      _tabbat: 0
+    })
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
